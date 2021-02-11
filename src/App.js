c7d8ad185b3506dc3a6fb9955341240134bafa23
JoaquinBarrachina <mechaximo@gmail.com>
2021-02-11 18:08:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import "./App.css";
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      setTimeout(async () => {
+        const res = await fetch(url);
+        const data = await res.json();
+        setJobs(data);
+        setLoading(false);
+      }, 1000);
+    } catch (e) {
+      console.error(e);
+      setLoading(false);
+      setJobs([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <section className="section loading">
+          <h1>Loading...</h1>
+        </section>
+      </>
+    );
+  }
+  /*Destructuring has to be done after the loading */
+  const { company, dates, duties, title } = jobs[value];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="section">
+        <div className="title">
+          <h2>Experience</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="jobs-center">
+          {/*btn container */}
+          <div className="btn-container">
+            {jobs.map((item, index) => {
+              return (
+                <button
+                  key={item.id}
+                  onClick={() => setValue(index)}
+                  className={`job-btn ${index === value ? "active-btn" : ""}`}
+                >
+                  {item.company}
+                </button>
+              );
+            })}
+          </div>
+          {/*Job article */}
+          <article className="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            <ul>
+              {duties.map((duty, index) => {
+                return (
+                  <div key={index} className="job-desc">
+                    <FaAngleDoubleRight className="job-icon" />
+                    <p>{duty}</p>
+                  </div>
+                );
+              })}
+            </ul>
+          </article>
+        </div>
+      </section>
+    </>
   );
 }
 
